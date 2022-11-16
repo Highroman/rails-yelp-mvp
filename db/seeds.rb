3255ea5e56169525d366c486f29e339934113fd3
Roman Bajine <bajine.roman@gmail.com>
2022-11-16 16:07:49 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "06060606060", category: "french" }
+pizza_west =  { name: "Pizza West", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "06060606060", category: "french" }
+mushroom = { name: "Mushroom", address: "Nirvana, allée du Paradis", phone_number: "666", category: "chinese" }
+bad_weed =  { name: "Bad Weed", address: "High high in the sky", phone_number: "06060606060", category: "french" }
+bellerose = { name: "Bellerose", address: "7 Boundary St, London E2 7JE", phone_number: "06060606060", category: "french" }
+
+[dishoom, pizza_west, mushroom, bad_weed, bellerose].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
